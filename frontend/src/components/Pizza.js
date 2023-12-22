5b1a7780a2b592312d2afa4203fb8edcdd1c9601
Elankumaran98 <siva98kumarane.com>
2023-12-22 11:02:31 +0530

    model is developed using react-bootstrap

--- frontend/src/components/Pizza.js
+++ frontend/src/components/Pizza.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
 const Pizza = ({ pizza }) => {
   const [quantity, setQuantity] = useState(1);
   const [varient, setVarient] = useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div
-      style={{ padding: "70px ",textAlign:"center" }}
+      style={{ padding: "70px ", textAlign: "center" }}
       className="shadow p-3 mb-5 bg-body-tertiary rounded">
-      <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        alt="e"
-        className="img-fluid"
-        style={{ height: "200px", width: "200px" }}
-      />
+      <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img
+          src={pizza.image}
+          alt="pizza"
+          className="img-fluid"
+          style={{ height: "200px", width: "200px" }}
+        />
+      </div>
+
       <div className="flex-container">
         <div className="w-100 m-1">
           <p>Varient</p>
@@ -45,12 +53,36 @@ const Pizza = ({ pizza }) => {
       </div>
       <div className="flex-container">
         <div className="w-100 m-1">
-          <h1 className="mt-1">Price :{pizza.prices[0][varient] * quantity} Rs/- </h1>
+          <h1 className="mt-1">
+            Price :{pizza.prices[0][varient] * quantity} Rs/-{" "}
+          </h1>
         </div>
         <div className="w-100 m-1">
           <button className="btn">Add To Cart</button>
         </div>
       </div>
+
+      <Modal show={show}>
+        <Modal.Header closeButton onClick={handleClose}>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body style={{textAlign: "center" }}>
+          <img
+            src={pizza.image}
+            alt="pizza"
+            className="img-fluid"
+            style={{ height: "300px", width: "300px" }}
+          />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn" onClick={handleClose}>
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
