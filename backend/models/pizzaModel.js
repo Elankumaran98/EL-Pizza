4d7d655486f05069a7609e7bbdf4dccfdad7ebf2
Elankumaran98 <siva98kumarane.com>
2023-12-22 13:15:51 +0530

    backend created and successfully connected mongodb

---
+++ backend/models/pizzaModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const pizzaSchema = mongoose.Schema({
+  name: { type: "string", required: true },
+  varients: [],
+  prices: [],
+  category: { type: "string", required: true },
+  image: { type: "string", required: true },
+  description: { type: "string", required: true },
+}, {
+    timestamps:true
+});
+
+
+const pizzaModel = mongoose.model('pizzas', pizzaSchema)
+
+module.exports=pizzaModel
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const connection = require("./db");
+const Pizza = require("./models/pizzaModel");
+const app = express();
+
+app.use(express.json());
+connection();
+app.get("/", (req, res) => {
+  res.send("Server is Working ........" + port);
+});
+
+app.get("/getpizzas", async (req, res) => {
+  try {
+    const pizzas = await Pizza.find({});
+    res.json(pizzas); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving pizzas"); 
+  }
+});
+
+
+const port = process.env.PORT || 9817;
+
+app.listen(port, () => {
+  "Server is Working ........ " + port;
+});
